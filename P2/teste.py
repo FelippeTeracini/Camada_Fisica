b90cb5c30d6efb12f01355d5e4109a2f76314f36
Lucas Vaz <lucastarraf@hotmail.com>
2018-08-17 14:23:30 -0300

    fon

--- P2/teste.py
+++ P2/teste.py
@@ -1,16 +1,17 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
-def filename():
-    root.fileName=filedialog.askopenfilename(filetype=(("All files","*.*"),))
+from aplicacaoTx import *
 
+def filename():
+    root.fileName=filedialog.askopenfilename(filetypes=(("All files",".jpg"),))
     x=root.fileName
-    print(x)
+    send(x)
     exit()
+print(serialName)
 root=Tk()
 root.geometry("600x600")
 btn1=ttk.Button(root,text="escolha a imagem")
 btn1.pack()
-
 btn1.config(command=filename)
 mainloop()
